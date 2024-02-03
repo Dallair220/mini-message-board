bf688d75bbfd14796518e2bd1a1115e6e799bba0
Dallair220 <lovance777@gmail.com>
2024-02-03 13:30:35 +0100

    Refactor / add routes and views to implement the mini message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Paul',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Jones',
+    added: new Date(),
+  },
+  {
+    text: 'Glad to be here!!',
+    user: 'Anna',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Chat with us!', messages });
+});
+
+router.post('/new', function (req, res, next) {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
